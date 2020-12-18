ef5e81e1a1f2ee3c9fa88b7ce51e9a69597b4cb3
jmvalmeria <68934751+jmvalmeria@users.noreply.github.com>
2020-12-18 18:06:30 -0500

    Second commit

--- src/test/java/com/myapp/runners/TestRunner.java
+++ src/test/java/com/myapp/runners/TestRunner.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+          "json:target/cucumber.json"
+        },
         features = "src/test/resources/features/Login.feature",
         glue = "com/myapp/steps_definitions",
         dryRun = false
